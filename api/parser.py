58425b8e0db643837df347d48dd7f62ee87216d2
Joshua Zhao <xinhua.zhao@hp.com>
2015-06-26 10:50:58 -0700

    Interface definition of REST meta schema parser.
    Change-Id: I67e259aec78b1b900d4f7a6cae1566e6b9ebc76f
    Signed-off-by: Joshua Zhao <xinhua.zhao@hp.com>

--- api/parser.py
+++ api/parser.py
@@ -28,8 +28,70 @@ import ovs.unixctl.server
 import ovs.vlog
 
 
+class OVSColumn(object):
+    """__init__() functions as the class constructor"""
+    def __init__(self, type, can_update = True):
+        # Possible values
+        self.enum = set([])
+
+        # For type Integer only
+        self.minInteger = None
+        self.maxInteger = None
+
+        # For type Real only
+        self.minReal = None
+        self.maxReal = None
+
+        # For type string only
+        self.minLength = None
+        self.maxLength = None
+
+        # The number of instances
+        self.is_list = False
+
+
+class OVSReference(object):
+    """__init__() functions as the class constructor"""
+    def __init__(self, table, relation = 'reference'):
+        # Name of the table being referenced
+        self.table = table
+
+        # Relationship of the referenced to the current table
+        # one of child, parent or reference
+        self.relation = relation
+
+        # The number of instances
+        self.is_plural = False
+
+
 class OVSTable(object):
     """__init__() functions as the class constructor"""
+    def __init__(self, index = 'name', is_many = True):
+        # What is the name of the index column
+        self.index = index
+
+        # Is the table in plural form?
+        self.is_many = is_many
+
+        # Dictionary of configuration attributes (RW)
+        # column name to OVSColumn object mapping
+        self.config = {}
+
+        # Dictionary of status attributes (Read-only)
+        # column name to OVSColumn object mapping
+        self.status = {}
+
+        # Dictionary of statistics attributes (Read-only)
+        # column name to OVSColumn object mapping
+        self.stats = {}
+
+        # Parent table name
+        self.parent = None
+
+        # List of table referenced
+        # table name to OVSReference object mapping
+        self.references = {}
 
 
-ovs_tables = []
+# A doctionary of table name to OVSTable object mappings
+ovs_tables = {}
