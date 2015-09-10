727995d25077b80073122c39cf0b6f4950e9997c
Mir M Ali <mir.ali@hpe.com>
2015-09-10 16:25:08 -0700

    Authentication using userauth module
    This is oleg's code for authenticating using userauth module
    for halon rest api
    
    Change-Id: Ia3c9fd0f5bba9a73ab7fb4b92f2998e20af4a8da
    Signed-off-by: Mir M Ali <mir.ali@hpe.com>

---
+++ tests/cookie-test.py
@@ -0,0 +1,28 @@
+import urllib, httplib2
+import json
+
+'''
+A simple test script to test authentication using the 'userauth' module.
+This is a 2 step test.
+
+Step 1: POST to /login to send username/password combination to fetch the secret cookie.
+step 2: Use the secret cookie to use authenticated session to GET /system information from the database.
+'''
+
+# POST to fetch cookie
+http = httplib2.Http()
+url = 'http://172.17.0.2:8091/login'
+body = {'username' : 'oleg', 'password' : 'oleg'}
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+response, content = http.request(url, 'POST', headers=headers, body=urllib.parse.urlencode(body))
+print(response)
+print(content)
+
+# set the cookie header before doing a GET
+headers = {'Cookie' : response['set-cookie']}
+
+# GET to fetch system info from the DB
+url = 'http://172.17.0.2:8091/rest/v1/system'
+response, content = http.request(url, 'GET', headers=headers)
+print(response)
+print(content)
