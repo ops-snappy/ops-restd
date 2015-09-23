25233bb010ede02fe3213536e69f9ed559b1f02c
olegspirt <oleg.spirt@hp.com>
2015-09-22 21:53:41 -0700

    Proper handling of the secret material
    Change-Id: Id834ab5b10a651b4fff1cac2041706793466b02d
    Signed-off-by: olegspirt <oleg.spirt@hp.com>

--- halonrest/application.py
+++ halonrest/application.py
@@ -8,10 +8,12 @@ from ovs.poller import Poller
 from halonrest.manager import OvsdbConnectionManager
 from halonlib import restparser
 from halonrest import constants
+import cookiesecret
 
 class OvsdbApiApplication(Application):
     def __init__(self, settings):
         self.settings = settings
+        self.settings['cookie_secret'] = cookiesecret.generate_cookie_secret()
         self.manager = OvsdbConnectionManager(self.settings.get('ovs_remote'), self.settings.get('ovs_schema'))
         self.restschema = restparser.parseSchema(self.settings.get('ext_schema'))
         self._url_patterns = self._get_url_patterns()
