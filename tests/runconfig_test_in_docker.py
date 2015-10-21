debef1588c8607a8f14b91758bf9e7893fb81813
Neeta <neeta.bhat@hp.com>
2015-10-20 21:12:46 -0400

    New input files nfig test script
    Change-Id: Ib3d47ddd731084712e828d38daad095d26648b12
    Signed-off-by: Neeta <neeta.bhat@hp.com>

--- tests/runconfig_test_in_docker.py
+++ tests/runconfig_test_in_docker.py
@@ -131,9 +131,9 @@ def test_write_read_compare(fname):
 if __name__ == "__main__":
     result = test_empty_config(CONFIG_FILENAME_2, CONFIG_FILENAME_3)
     if result:
-        pass #result = test_write_read_compare(CONFIG_FILENAME_1)
+        result = test_write_read_compare(CONFIG_FILENAME_1)
         if result:
-            pass #result = test_write_read_compare(CONFIG_FILENAME_2)
+            result = test_write_read_compare(CONFIG_FILENAME_2)
         else:
             print "Test Failure"
     else:
