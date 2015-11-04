4bd19a24dbb2d473b2ffca48836289695a32d541
Chinh Le <chinh.le@hpe.com>
2015-11-04 12:53:01 -0800

    REST/Swagger - Fix map rendering in Swagger.
    Maps are now rendered with key type and value type
    for maps that do not have keys defined in the schema
    or XML definition file.
    
    Taiga Issue 85.
    
    Change-Id: I49aa1c8136afb2ded3fc504ca1ff65db4ebd8e25
    Signed-off-by: Chinh Le <chinh.le@hpe.com>

--- opslib/apidocgen.py
+++ opslib/apidocgen.py
@@ -295,33 +295,38 @@ def genDelInstance(table, parent_plurality, is_plural):
     return op
 
 
-# Generate definition for a given base type
-def genBaseType(type, min, max, desc):
-    item = {}
+# Gets the correlated Swagger representation of primitive data types.
+def getDataType(type):
     if type == types.IntegerType:
-        item["type"] = "integer"
-        if min is not None and min is not 0:
-            item["minimum"] = min
-        if max is not None and max is not sys.maxint:
-            item["maximum"] = max
+        return "integer"
     elif type == types.RealType:
-        item["type"] = "real"
-        if min is not None and min is not 0:
-            item["minimum"] = min
-        if max is not None and max is not sys.maxint:
-            item["maximum"] = max
+        return "real"
     elif type == types.BooleanType:
-        item["type"] = "boolean"
+        return "boolean"
     elif type == types.StringType:
-        item["type"] = "string"
-        if min is not None and min is not 0:
-            item["minLength"] = min
-        if max is not None and max is not sys.maxint:
-            item["maxLength"] = max
+        return "string"
     else:
         raise error.Error("Unexpected attribute type " + type)
 
+
+# Generate definition for a given base type
+def genBaseType(type, min, max, desc):
+    item = {}
+    item["type"] = getDataType(type)
     item["description"] = desc
+
+    if type != types.BooleanType:
+        if type == types.StringType:
+            minStr = "minLength"
+            maxStr = "maxLength"
+        else:
+            minStr = "minimum"
+            maxStr = "maximum"
+
+        if min is not None and min is not 0:
+            item[minStr] = min
+        if max is not None and max is not sys.maxint:
+            item[maxStr] = max
 
     return item
 
@@ -357,6 +362,17 @@ def genDefinition(table_name, col, definitions):
                 desc = ""
             properties[key] = genBaseType(type, min, max, desc)
 
+    if not properties and col.is_dict:
+        # Some maps in the schema do not have keys defined (i.e. external_ids).
+        # If keys are not defined, the type of the key (i.e. "string") should
+        # be rendered as the key, and the type of the value (i.e. "string")
+        # should be rendered as the value.
+        key_type = getDataType(col.type)
+        properties[key_type] = genBaseType(col.value_type,
+                                           col.valueRangeMin,
+                                           col.valueRangeMax,
+                                           "Key-Value pair for " + col.name)
+
     if properties:
         definitions[table_name + "-" + col.name + "-KV"] = {"properties":
                                                             properties}
@@ -390,7 +406,7 @@ def getDefinition(schema, table, definitions):
                 sub["items"] = item
             else:
                 sub["$ref"] = "#/definitions/Resource"
-                sub["description"] = "Referece of " + child_table.name
+                sub["description"] = "Reference of " + child_table.name
             properties[col_name] = sub
 
     definitions[table.name + "Config"] = {"properties": properties}
@@ -405,7 +421,7 @@ def getDefinition(schema, table, definitions):
             subtable_name = col_name
         sub = {}
         sub["$ref"] = "#/definitions/" + subtable_name + "ConfigData"
-        sub["description"] = "Referenced resurce of " + subtable_name + " instances"
+        sub["description"] = "Referenced resource of " + subtable_name + " instances"
         properties[col_name] = sub
 
     # Special treat /system resource
@@ -421,7 +437,7 @@ def getDefinition(schema, table, definitions):
 
             sub = {}
             sub["$ref"] = "#/definitions/" + subtable.name + "ConfigData"
-            sub["description"] = "Referenced resurce of " + subtable.name + " instances"
+            sub["description"] = "Referenced resource of " + subtable.name + " instances"
             properties[subtable_name] = sub
 
     definitions[table.name + "ConfigFull"] = {"properties": properties}
