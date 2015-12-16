OpenSwitchNOS/openswitch-ops-restd

new: dev: REST: User Management Resource
Add support for user management (Add, delete, update and query)
New request handler for custom resources

Change-Id: Id64156e9df47edc502583961efd04dfa36f059eb
Signed-off-by: Daniela Ramírez <daniela.rch@hpe.com>

opsrest/custom/user_controller.py
@@ -1,0 +1,172 @@
+# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+# Third party imports
+import pwd
+import crypt
+import os
+import base64
+
+from subprocess import call
+
+# Local imports
+import opsrest.custom.user_utils as user_utils
+
+from opsrest.utils.utils import to_json_error
+from opsrest.constants import ERROR
+from opsrest.custom.schema_validator import SchemaValidator
+from opsrest.custom.base_controller import BaseController
+from opsrest.custom.rest_object import RestObject
+from opsrest.custom.user_validator import UserValidator
+from opsrest.constants import OVSDB_SCHEMA_CONFIG, DEFAULT_USER_GRP
+from opsrest.custom.base_controller import OP_CREATE, OP_UPDATE
+
+
+class UserController(BaseController):
+
+    def __init__(self):
+        self.schema_validator = SchemaValidator("user_schema")
+        self.validator = UserValidator()
+
+    def __get_encrypted_password__(self, user):
+        """
+        Encrypts the user password using SHA-256 and
+        base 64 salt (username + random value)
+        Returns encrypted password
+        """
+        salt_data = user.configuration.username.encode("utf8") + os.urandom(16)
+        salt = base64.b64encode(salt_data)
+        encoded_password = crypt.crypt(user.configuration.password,
+                                       "$6$%s$" % salt)
+        return encoded_password
+
+    def __fill_user_data__(self, user_data, selector):
+        """
+        Encapsulates user information inside RestObject instance
+        Returns RestObject instance
+        """
+        # TODO create empty object using schema definition
+        user_dict = RestObject.create_empty_json(selector)
+        if OVSDB_SCHEMA_CONFIG in user_dict:
+            user_dict[OVSDB_SCHEMA_CONFIG]["username"] = user_data.pw_name
+
+        user_instance = RestObject.from_json(user_dict)
+        return user_instance
+
+    def create(self, data, current_user):
+        """
+        Create user at ovsdb_users group
+        Returns result dictionary
+        """
+        # Validate json
+        validation_result = self.schema_validator.validate_json(data,
+                                                                OP_CREATE)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        # Validate user
+        user = RestObject.from_json(data)
+        validation_result = self.validator.validate_create(user, current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        username = user.configuration.username
+        encoded_password = self.__get_encrypted_password__(user)
+        result = {}
+        try:
+            cmd_result = call(["sudo", "useradd", username, "-p",
+                               encoded_password, "-g", DEFAULT_USER_GRP])
+            if cmd_result == 0:
+                result = {"key": username}
+            else:
+                error_json = to_json_error("User %s not added" % username,
+                                           None, None)
+                result = {ERROR: error_json}
+        except KeyError:
+            error_json = to_json_error("An error ocurred", None, None)
+            result = {ERROR: error_json}
+
+        return result
+
+    def update(self, item_id, data, current_user):
+        """
+        Update user from ovsdb_users group
+        Returns result dictionary
+        """
+        validation_result = self.schema_validator.validate_json(data,
+                                                                OP_UPDATE)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        data[OVSDB_SCHEMA_CONFIG]["username"] = item_id
+        user = RestObject.from_json(data)
+        validation_result = self.validator.validate_update(user, current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        username = user.configuration.username
+        encoded_password = self.__get_encrypted_password__(user)
+        result = {}
+        try:
+            cmd_result = call(["sudo", "usermod", "-p",
+                               encoded_password, username])
+            if cmd_result != 0:
+                error_json = to_json_error("User %s not modified" % username,
+                                           None, None)
+                result = {ERROR: error_json}
+        except KeyError:
+            error_json = to_json_error("An error ocurred", None, None)
+            result = {ERROR: error_json}
+        return result
+
+    def delete(self, item_id, current_user):
+        """
+        Delete user from ovsdb_users group
+        Returns result dictionary
+        """
+        username = item_id
+        validation_result = self.validator.validate_delete(username,
+                                                           current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        cmd_result = call(["sudo", "userdel", "-r", username], shell=False)
+        result = {}
+        if cmd_result != 0:
+            if cmd_result == 8:
+                error_message = "User %s currently logged in." % username
+                error_json = to_json_error(error_message, None, None)
+                return {ERROR: error_json}
+            else:
+                error_json = to_json_error("User %s not deleted." % username,
+                                           None, None)
+                return {ERROR: error_json}
+        return result
+
+    def get_all(self, current_user, selector=None, query_args=None):
+        """
+        Retrieve all users from ovsdb_users group
+        Return users dictionary list
+        """
+        group_id = user_utils.get_group_id(DEFAULT_USER_GRP)
+        all_users = []
+        users = pwd.getpwall()
+        for user_data in users:
+            if user_data.pw_gid == group_id:
+                user_instance = self.__fill_user_data__(user_data, selector)
+                if user_instance:
+                    all_users.append(user_instance)
+
+        result = RestObject.to_json_list(all_users)
+        return result

opsrest/custom/user_validator.py
@@ -1,0 +1,156 @@
+# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+# Third party imports
+import re
+
+# Local imports
+import opsrest.custom.user_utils as user_utils
+
+from opsrest.utils.utils import to_json_error
+from opsrest.constants import DEFAULT_USER_GRP
+from opsrest.custom.base_controller import OP_CREATE, OP_UPDATE
+
+'''
+Constants
+'''
+USERNAME_REGEX = r'^[a-z_][a-z0-9_-]*[$]?$'
+REQUIRED_FIELD_MESSAGE = "Attribute is missing from request"
+
+
+class UserValidator():
+
+    def __validate_required_fields__(self, user, operation):
+        """
+        Validate required user fields
+        Returns None when valid else returns error json dict
+        """
+        if operation == OP_CREATE and \
+                (not hasattr(user.configuration, "username") or
+                 user.configuration.username is None):
+            validation_result = to_json_error(REQUIRED_FIELD_MESSAGE,
+                                              None, "username")
+            return validation_result
+
+        if not hasattr(user.configuration, "password") or \
+                user.configuration.password is None:
+            validation_result = to_json_error(REQUIRED_FIELD_MESSAGE,
+                                              None, "password")
+            return validation_result
+        return None
+
+    def __validate_username__(self, username):
+        """
+        Validate username using a regular expression
+        Returns None when valid else returns error json dict
+        """
+        re_result = re.match(USERNAME_REGEX, username)
+        if not re.match(USERNAME_REGEX, username) \
+                and username == re_result.group():
+            validation_result = to_json_error("Invalid username",
+                                              None, "username")
+            return validation_result
+
+        return None
+
+    def validate_create(self, user, current_user):
+        """
+        Validate required fields, username and if the user exists
+        Returns None when valid else returns error json dict
+        """
+        validation_result = self.__validate_required_fields__(user, OP_CREATE)
+        if validation_result is not None:
+            return validation_result
+
+        username = user.configuration.username
+        validation_result = self.__validate_username__(username)
+        if validation_result is not None:
+            return validation_result
+
+        if user_utils.user_exists(username):
+            error_message = "User %s already exists" % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+    def validate_update(self, user, current_user):
+        """
+        Validates required fields, username, verifies if the user
+        exists, verifies that the user is not root and that belongs to
+        ovsdb_user group
+        Returns None when valid else returns error json dict
+        """
+        validation_result = self.__validate_required_fields__(user, OP_UPDATE)
+        if validation_result is not None:
+            return validation_result
+
+        username = user.configuration.username
+        validation_result = self.__validate_username__(username)
+        if validation_result is not None:
+            return validation_result
+
+        if user_utils.user_exists(username):
+            # Avoid update a root user
+            if username == "root":
+                error_message = "Permission denied."\
+                                "Cannot update the root user."
+                validation_result = to_json_error(error_message, None, None)
+                return validation_result
+            # Cannot update a user from ovsdb_users group
+            if not user_utils.check_user_group(username, DEFAULT_USER_GRP):
+                error_message = "Unknown user %s" % username
+                validation_result = to_json_error(error_message, None, None)
+                return validation_result
+        else:
+            error_message = "User %s doesn't exists." % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        return None
+
+    def validate_delete(self, username, current_user):
+        """
+        This functions verifies the following:
+        User is not root
+        User is not the current user
+        User belongs to ovsdb_user group
+        User is not the last user at ovsdb_group
+        Returns None when valid else returns error json dict
+        """
+        # Avoid delete a root user
+        if username == "root":
+            error_message = "Permission denied." \
+                            "Cannot remove the root user."
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        # Avoid to delete the current user
+        if username == current_user["username"]:
+            error_message = "Permission denied." \
+                            "Cannot remove the current user."
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        # Avoid delete system users.
+        if not user_utils.check_user_group(username, DEFAULT_USER_GRP):
+            validation_result = to_json_error("Unknown user %s" % username,
+                                              None, None)
+            return validation_result
+
+        # Check if deleting the last user from that group
+        if user_utils.get_group_user_count(DEFAULT_USER_GRP) <= 1:
+            validation_result = "Cannot delete the last user %s" % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        return None

opsrest/handlers/base.py
@@ -29,6 +29,8 @@ from opsrest import get, post, delete, put
 import userauth
 from opsrest.settings import settings
 
+# TODO refactor common handler functions
+
 
 class BaseHandler(web.RequestHandler):
 
@@ -53,6 +55,9 @@ class BaseHandler(web.RequestHandler):
         if HTTP_HEADER_ORIGIN in self.request.headers:
             self.set_header("Access-Control-Allow-Origin",
                             self.request.headers[HTTP_HEADER_ORIGIN])
+
+    def get_current_user(self):
+        return userauth.get_authenticated_user(self)
 
 
 class LoginHandler(BaseHandler):

ops-snappy/ops-restd

opsrest/custom/user_controller.py
@@ -1,0 +1,172 @@
+# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+# Third party imports
+import pwd
+import crypt
+import os
+import base64
+
+from subprocess import call
+
+# Local imports
+import opsrest.utils.user_utils as user_utils
+
+from opsrest.utils.utils import to_json_error
+from opsrest.constants import ERROR
+from opsrest.custom.schema_validator import SchemaValidator
+from opsrest.custom.base_controller import BaseController
+from opsrest.custom.rest_object import RestObject
+from opsrest.custom.user_validator import UserValidator
+from opsrest.constants import OVSDB_SCHEMA_CONFIG, DEFAULT_USER_GRP
+from opsrest.custom.base_controller import OP_CREATE, OP_UPDATE
+
+
+class UserController(BaseController):
+
+    def __init__(self):
+        self.schema_validator = SchemaValidator("user_schema")
+        self.validator = UserValidator()
+
+    def __get_encrypted_password__(self, user):
+        """
+        Encrypts the user password using SHA-256 and
+        base 64 salt (username + random value)
+        Returns encrypted password
+        """
+        salt_data = user.configuration.username.encode("utf8") + os.urandom(16)
+        salt = base64.b64encode(salt_data)
+        encoded_password = crypt.crypt(user.configuration.password,
+                                       "$6$%s$" % salt)
+        return encoded_password
+
+    def __fill_user_data__(self, user_data, selector):
+        """
+        Encapsulates user information inside RestObject instance
+        Returns RestObject instance
+        """
+        # TODO create empty object using schema definition
+        user_dict = RestObject.create_empty_json(selector)
+        if OVSDB_SCHEMA_CONFIG in user_dict:
+            user_dict[OVSDB_SCHEMA_CONFIG]["username"] = user_data.pw_name
+
+        user_instance = RestObject.from_json(user_dict)
+        return user_instance
+
+    def create(self, data, current_user):
+        """
+        Create user at ovsdb_users group
+        Returns result dictionary
+        """
+        # Validate json
+        validation_result = self.schema_validator.validate_json(data,
+                                                                OP_CREATE)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        # Validate user
+        user = RestObject.from_json(data)
+        validation_result = self.validator.validate_create(user, current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        username = user.configuration.username
+        encoded_password = self.__get_encrypted_password__(user)
+        result = {}
+        try:
+            cmd_result = call(["sudo", "useradd", username, "-p",
+                               encoded_password, "-g", DEFAULT_USER_GRP])
+            if cmd_result == 0:
+                result = {"key": username}
+            else:
+                error_json = to_json_error("User %s not added" % username,
+                                           None, None)
+                result = {ERROR: error_json}
+        except KeyError:
+            error_json = to_json_error("An error ocurred", None, None)
+            result = {ERROR: error_json}
+
+        return result
+
+    def update(self, item_id, data, current_user):
+        """
+        Update user from ovsdb_users group
+        Returns result dictionary
+        """
+        validation_result = self.schema_validator.validate_json(data,
+                                                                OP_UPDATE)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        data[OVSDB_SCHEMA_CONFIG]["username"] = item_id
+        user = RestObject.from_json(data)
+        validation_result = self.validator.validate_update(user, current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        username = user.configuration.username
+        encoded_password = self.__get_encrypted_password__(user)
+        result = {}
+        try:
+            cmd_result = call(["sudo", "usermod", "-p",
+                               encoded_password, username])
+            if cmd_result != 0:
+                error_json = to_json_error("User %s not modified" % username,
+                                           None, None)
+                result = {ERROR: error_json}
+        except KeyError:
+            error_json = to_json_error("An error ocurred", None, None)
+            result = {ERROR: error_json}
+        return result
+
+    def delete(self, item_id, current_user):
+        """
+        Delete user from ovsdb_users group
+        Returns result dictionary
+        """
+        username = item_id
+        validation_result = self.validator.validate_delete(username,
+                                                           current_user)
+        if validation_result is not None:
+            return {ERROR: validation_result}
+
+        cmd_result = call(["sudo", "userdel", "-r", username], shell=False)
+        result = {}
+        if cmd_result != 0:
+            if cmd_result == 8:
+                error_message = "User %s currently logged in." % username
+                error_json = to_json_error(error_message, None, None)
+                return {ERROR: error_json}
+            else:
+                error_json = to_json_error("User %s not deleted." % username,
+                                           None, None)
+                return {ERROR: error_json}
+        return result
+
+    def get_all(self, current_user, selector=None, query_args=None):
+        """
+        Retrieve all users from ovsdb_users group
+        Return users dictionary list
+        """
+        group_id = user_utils.get_group_id(DEFAULT_USER_GRP)
+        all_users = []
+        users = pwd.getpwall()
+        for user_data in users:
+            if user_data.pw_gid == group_id:
+                user_instance = self.__fill_user_data__(user_data, selector)
+                if user_instance:
+                    all_users.append(user_instance)
+
+        result = RestObject.to_json_list(all_users)
+        return result

opsrest/custom/user_validator.py
@@ -1,0 +1,155 @@
+# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+# Third party imports
+import re
+
+# Local imports
+import opsrest.utils.user_utils as user_utils
+
+from opsrest.utils.utils import to_json_error
+from opsrest.constants import DEFAULT_USER_GRP
+from opsrest.custom.base_controller import OP_CREATE, OP_UPDATE
+
+'''
+Constants
+'''
+USERNAME_REGEX = r'^[a-z_][a-z0-9_-]*[$]?$'
+REQUIRED_FIELD_MESSAGE = "Attribute is missing from request"
+
+
+class UserValidator():
+
+    def __validate_required_fields__(self, user, operation):
+        """
+        Validate required user fields
+        Returns None when valid else returns error json dict
+        """
+        if operation == OP_CREATE and \
+                (not hasattr(user.configuration, "username") or
+                 user.configuration.username is None):
+            validation_result = to_json_error(REQUIRED_FIELD_MESSAGE,
+                                              None, "username")
+            return validation_result
+
+        if not hasattr(user.configuration, "password") or \
+                user.configuration.password is None:
+            validation_result = to_json_error(REQUIRED_FIELD_MESSAGE,
+                                              None, "password")
+            return validation_result
+        return None
+
+    def __validate_username__(self, username):
+        """
+        Validate username using a regular expression
+        Returns None when valid else returns error json dict
+        """
+        re_result = re.match(USERNAME_REGEX, username)
+        if not re_result or username != re_result.group():
+            validation_result = to_json_error("Invalid username",
+                                              None, "username")
+            return validation_result
+
+        return None
+
+    def validate_create(self, user, current_user):
+        """
+        Validate required fields, username and if the user exists
+        Returns None when valid else returns error json dict
+        """
+        validation_result = self.__validate_required_fields__(user, OP_CREATE)
+        if validation_result is not None:
+            return validation_result
+
+        username = user.configuration.username
+        validation_result = self.__validate_username__(username)
+        if validation_result is not None:
+            return validation_result
+
+        if user_utils.user_exists(username):
+            error_message = "User %s already exists" % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+    def validate_update(self, user, current_user):
+        """
+        Validates required fields, username, verifies if the user
+        exists, verifies that the user is not root and that belongs to
+        ovsdb_user group
+        Returns None when valid else returns error json dict
+        """
+        validation_result = self.__validate_required_fields__(user, OP_UPDATE)
+        if validation_result is not None:
+            return validation_result
+
+        username = user.configuration.username
+        validation_result = self.__validate_username__(username)
+        if validation_result is not None:
+            return validation_result
+
+        if user_utils.user_exists(username):
+            # Avoid update a root user
+            if username == "root":
+                error_message = "Permission denied."\
+                                "Cannot update the root user."
+                validation_result = to_json_error(error_message, None, None)
+                return validation_result
+            # Avoid update users from another group
+            if not user_utils.check_user_group(username, DEFAULT_USER_GRP):
+                error_message = "Unknown user %s" % username
+                validation_result = to_json_error(error_message, None, None)
+                return validation_result
+        else:
+            error_message = "User %s doesn't exists." % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        return None
+
+    def validate_delete(self, username, current_user):
+        """
+        This functions verifies the following:
+        User is not root
+        User is not the current user
+        User belongs to ovsdb_user group
+        User is not the last user at ovsdb_group
+        Returns None when valid else returns error json dict
+        """
+        # Avoid delete a root user
+        if username == "root":
+            error_message = "Permission denied." \
+                            "Cannot remove the root user."
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        # Avoid to delete the current user
+        if username == current_user["username"]:
+            error_message = "Permission denied." \
+                            "Cannot remove the current user."
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        # Avoid delete system users.
+        if not user_utils.check_user_group(username, DEFAULT_USER_GRP):
+            validation_result = to_json_error("Unknown user %s" % username,
+                                              None, None)
+            return validation_result
+
+        # Check if deleting the last user from that group
+        if user_utils.get_group_user_count(DEFAULT_USER_GRP) <= 1:
+            validation_result = "Cannot delete the last user %s" % username
+            validation_result = to_json_error(error_message, None, None)
+            return validation_result
+
+        return None

opsrest/handlers/base.py
@@ -31,6 +31,8 @@ from opsrest import get, post, delete, put
 import userauth
 from opsrest.settings import settings
 
+# TODO refactor common handler functions
+
 
 class BaseHandler(web.RequestHandler):
 
@@ -55,6 +57,9 @@ class BaseHandler(web.RequestHandler):
         if HTTP_HEADER_ORIGIN in self.request.headers:
             self.set_header("Access-Control-Allow-Origin",
                             self.request.headers[HTTP_HEADER_ORIGIN])
+
+    def get_current_user(self):
+        return userauth.get_request_user(self)
 
 
 class LoginHandler(BaseHandler):