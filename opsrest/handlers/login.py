ef9dfc732f256e2329a00249efb7fe5b4f85a114
Eduardo Ramirez <eramireza@hpe.com>
2016-01-18 12:31:25 -0800

    chg: dev: Separate handlers into individual files
    As part of fix for bug #156, handlers' code is
    now separated into individual files, this is the
    first step into eliminating duplicated code and
    supporting filtering, sorting and pagination for
    all handlers/resources.
    
    Change-Id: Ie7005654dcac593e4a405eb3f9bf7c06846225c6
    Signed-off-by: Eduardo Ramirez <eramireza@hpe.com>
    (cherry picked from commit 0e1d0faf7baa0f57491969863fbf704b60b7a73d)

---
+++ opsrest/handlers/login.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from tornado import gen
+from tornado.log import app_log
+
+import httplib
+import userauth
+
+from opsrest.handlers import base
+
+
+class LoginHandler(base.BaseHandler):
+
+    # pass the application reference to the handlers
+    def initialize(self, ref_object):
+        pass
+
+    @gen.coroutine
+    def get(self):
+
+        app_log.debug("Executing Login GET...")
+
+        is_authenticated = userauth.is_user_authenticated(self)
+        if not is_authenticated:
+            self.set_status(httplib.UNAUTHORIZED)
+            self.set_header("Link", "/login")
+        else:
+            self.set_status(httplib.OK)
+
+        self.finish()
+
+    @gen.coroutine
+    def post(self):
+
+        app_log.debug("Executing Login POST...")
+
+        login_success = userauth.handle_user_login(self)
+        if not login_success:
+            self.set_status(httplib.UNAUTHORIZED)
+            self.set_header("Link", "/login")
+        else:
+            self.set_status(httplib.OK)
+
+        self.finish()
