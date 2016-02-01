OpenSwitchNOS/openswitch-ops-restd

fix:usr: Enable HTTP along with HTTPS mode for REST.
Change-Id: Ibd82ba9b732b03ff7c7a5187c4aa0d210f5d7b99
Signed-off-by: Madhulika <madhulika.madishetty@hpe.com>

restd.py
@@ -22,12 +22,17 @@ def main():
 
     app_log.debug("Creating OVSDB API Application!")
     app = OvsdbApiApplication(settings)
-    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
+    HTTPS_server = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile":"/etc/ssl/certs/server.crt",
         "keyfile":"/etc/ssl/certs/server-private.key"})
 
+    HTTP_server = tornado.httpserver.HTTPServer(app)
+
     app_log.debug("Server listening to port: %s" % options.https_port)
-    http_server.listen(options.https_port)
+    HTTPS_server.listen(options.https_port)
+
+    app_log.debug("Server listening to port: %s" % options.http_port)
+    HTTP_server.listen(options.http_port)
 
     app_log.info("Starting server!")
     tornado.ioloop.IOLoop.instance().start()

ops-snappy/ops-restd

restd.py
@@ -22,12 +22,17 @@ def main():
 
     app_log.debug("Creating OVSDB API Application!")
     app = OvsdbApiApplication(settings)
-    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
+    HTTPS_server = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile":"/etc/ssl/certs/server.crt",
         "keyfile":"/etc/ssl/certs/server-private.key"})
 
-    app_log.debug("Server listening to port: %s" % options.https_port)
-    http_server.listen(options.https_port)
+    HTTP_server = tornado.httpserver.HTTPServer(app)
+
+    app_log.debug("Server listening to port: %s" % options.HTTPS_port)
+    HTTPS_server.listen(options.HTTPS_port)
+
+    app_log.debug("Server listening to port: %s" % options.HTTP_port)
+    HTTP_server.listen(options.HTTP_port)
 
     app_log.info("Starting server!")
     tornado.ioloop.IOLoop.instance().start()