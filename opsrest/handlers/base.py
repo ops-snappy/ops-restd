4290eb9c8553ca8fffb35f13cadb82c53713d84b
Eduardo Ramirez <eramireza@hpe.com>
2016-02-19 07:38:29 -0800

    fix: usr: Selector is allowed in POST/PUT/PATCH
    In order to guarantee deterministic etag calculation
    for configuration attributes, POST/PUT/PATCH should
    allow the selector URI argument for If-Match processing.
    
    Change-Id: I4fb61fe3bdb24941fdf809ce9656f16ffc333496
    Signed-off-by: Eduardo Ramirez <eramireza@hpe.com>

--- opsrest/handlers/base.py
+++ opsrest/handlers/base.py
@@ -74,9 +74,6 @@ class BaseHandler(web.RequestHandler):
             sort = get_query_arg(REST_QUERY_PARAM_SORTING,
                                  self.request.query_arguments)
 
-            selector = get_query_arg(REST_QUERY_PARAM_SELECTOR,
-                                     self.request.query_arguments)
-
             depth = get_query_arg(REST_QUERY_PARAM_DEPTH,
                                   self.request.query_arguments)
 
@@ -85,11 +82,10 @@ class BaseHandler(web.RequestHandler):
 
             if self.request.method != REQUEST_TYPE_READ \
                and (depth is not None or columns is not None or
-                    sort is not None or selector is not None):
-                raise ParameterNotAllowed("arguments %s, %s, %s and %s "
+                    sort is not None):
+                raise ParameterNotAllowed("Arguments %s, %s and %s "
                                           "are only allowed in %s" %
                                           (REST_QUERY_PARAM_SORTING,
-                                           REST_QUERY_PARAM_SELECTOR,
                                            REST_QUERY_PARAM_DEPTH,
                                            REST_QUERY_PARAM_COLUMNS,
                                            REQUEST_TYPE_READ))
